044677b23152da9ae43f7ff56e50c6fa46f747a9
Rafael <silva.rafa@hotmail.com>
2022-03-28 16:11:40 -0300

    Criada função para buscar avatar de usuário no github

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -9,7 +9,7 @@ def buscar_avatar(usuario):
     :return: str com o link do avatar
     """
     url = f'https://api.github.com/users/{usuario}'
-    resp = requests.get(url)99
+    resp = requests.get(url)
     return resp.json()['avatar_url']
 
 if __name__ == '__main__':
